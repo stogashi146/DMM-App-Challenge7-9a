e5972f549ae0d2e08f280c32be62523bd35b0e1d
stogashi146 <stogashi146@gmail.com>
2021-07-06 07:31:27 +0000

    [Start]App-Challenge7-9a

---
+++ app/models/book.rb
@@ -0,0 +1,25 @@
+class Book < ApplicationRecord
+	belongs_to :user
+	has_many :favorites,dependent: :destroy
+	has_many :post_comments,dependent: :destroy
+
+	# 引数で渡されたユーザーIDが、Favoritesテーブル内に存在するか調べる
+	def favorited_by?(user)
+		favorites.where(user_id:user.id).exists?
+	end
+
+	validates :title, presence: true
+	validates :body, presence: true, length: {maximum: 200}
+
+	def self.search_for(content, method)
+		if method == "perfect"
+      Book.where(title: content)
+    elsif method == "partial"
+      Book.where("title LIKE ?","%" + content + "%")
+    elsif method == "forward"
+      Book.where("title LIKE ?",content + "%")
+    elsif method == "backward"
+      Book.where("title LIKE ?","%" + content)
+     end
+   end
+end
