dd58e547d729b17201f34f160ff6ab133c1795a9
stogashi146 <stogashi146@gmail.com>
2021-07-10 09:27:43 +0000

    [Update]7d

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_072047) do
+ActiveRecord::Schema.define(version: 2021_07_10_081054) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_07_10_072047) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rate"
   end
 
   create_table "chats", force: :cascade do |t|
